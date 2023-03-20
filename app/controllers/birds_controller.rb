e5ed39fa353ba8e7bca02f58744104c5573a0ba0
edna1996 <guantaiedna@gmapl.com>
2023-03-20 11:14:34 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,21 +1,38 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
-
   # POST /birds
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+  def update
+    # find
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      # update
+      bird.update(bird_params)
+      render json: bird, status: :accepted
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
       render json: bird
     else
       render json: { error: "Bird not found" }, status: :not_found
@@ -26,6 +43,7 @@ class BirdsController < ApplicationController
 
   def bird_params
     params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
